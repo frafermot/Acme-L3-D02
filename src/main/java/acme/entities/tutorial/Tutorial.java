f98469d6af33df6203af1c00294ee73ce4246b59
Francisco Fernández Mota <frafermot@alum.us.es>
2023-03-15 20:56:29 +0100

    edit

--- src/main/java/acme/entities/tutorial/Tutorial.java
+++ src/main/java/acme/entities/tutorial/Tutorial.java
@@ -3,13 +3,19 @@ package acme.entities.tutorial;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 
 import org.hibernate.validator.constraints.Length;
 
+import acme.entities.course.Course;
 import acme.framework.data.AbstractEntity;
+import acme.roles.Assistant;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -39,7 +45,6 @@ public class Tutorial extends AbstractEntity {
 
 	@Positive
 	protected double			estimatedTime;
-
 
 	@NotNull
 	@Valid
