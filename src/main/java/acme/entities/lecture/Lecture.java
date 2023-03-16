4f2d75b7b46785582484df5a2ab9f4d00f47b0fa
antoniopelaezmoreno <antpelmor@alum.us.es>
2023-03-16 18:38:53 +0100

    Course-Lecture relation fixed

--- src/main/java/acme/entities/lecture/Lecture.java
+++ src/main/java/acme/entities/lecture/Lecture.java
@@ -2,16 +2,12 @@
 package acme.entities.lecture;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.entities.course.Course;
 import acme.enums.Indication;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
@@ -48,9 +44,4 @@ public class Lecture extends AbstractEntity {
 	protected String			link;
 
 	protected boolean			published;
-
-	@NotNull
-	@Valid
-	@ManyToOne(optional = false)
-	protected Course			course;
 }
