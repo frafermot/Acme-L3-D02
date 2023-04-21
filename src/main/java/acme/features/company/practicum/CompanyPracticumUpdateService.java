fa2879d00cb4c24a0f02096964637ba3558578ec
Francisco Fernández Mota <frafermot@alum.us.es>
2023-04-21 02:22:47 +0200

    update practicum, menos que conserve el code propio

--- src/main/java/acme/features/company/practicum/CompanyPracticumUpdateService.java
+++ src/main/java/acme/features/company/practicum/CompanyPracticumUpdateService.java
@@ -1,13 +1,105 @@
 
 package acme.features.company.practicum;
 
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import acme.entities.course.Course;
 import acme.entities.practicum.Practicum;
+import acme.enums.Indication;
+import acme.framework.components.jsp.SelectChoices;
+import acme.framework.components.models.Tuple;
 import acme.framework.services.AbstractService;
 import acme.roles.Company;
 
 @Service
 public class CompanyPracticumUpdateService extends AbstractService<Company, Practicum> {
 
+	@Autowired
+	protected CompanyPracticumRepository repository;
+
+
+	@Override
+	public void check() {
+		boolean status;
+
+		status = super.getRequest().hasData("id", int.class);
+
+		super.getResponse().setChecked(status);
+	}
+
+	@Override
+	public void authorise() {
+		Practicum object;
+		int id;
+		boolean status;
+		Company company;
+
+		company = this.repository.findCompanyByUserId(super.getRequest().getPrincipal().getAccountId());
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findPracticumById(id);
+		status = object != null && super.getRequest().getPrincipal().hasRole(Company.class) && object.getCompany().getId() == company.getId();
+		super.getResponse().setAuthorised(status && !object.isPublished());
+	}
+
+	@Override
+	public void load() {
+		Practicum object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findPracticumById(id);
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void bind(final Practicum object) {
+		assert object != null;
+
+		super.bind(object, "code", "title", "practicumAbstract", "goals", "estimatedTotalTime", "published");
+	}
+
+	@Override
+	public void validate(final Practicum object) {
+		assert object != null;
+
+		if (!super.getBuffer().getErrors().hasErrors("code")) {
+			Optional<Practicum> optPracticum;
+
+			optPracticum = this.repository.findPracticumByCode(object.getCode());
+			if (optPracticum.isPresent())
+				super.state(optPracticum == null, "code", "company.practicum.form.error.duplicated");
+
+		}
+	}
+
+	@Override
+	public void perform(final Practicum object) {
+		assert object != null;
+
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final Practicum object) {
+		assert object != null;
+
+		final SelectChoices choice;
+		final Collection<Course> courses;
+		final Tuple tuple;
+
+		courses = this.repository.findAllCourses().stream().filter(x -> !x.getIndicator().equals(Indication.THEORETICAL)).collect(Collectors.toList());
+		choice = SelectChoices.from(courses, "title", object.getCourse());
+
+		tuple = super.unbind(object, "code", "title", "practicumAbstract", "goals", "estimatedTotalTime", "published", "company");
+		tuple.put("courses", choice);
+		tuple.put("company", object.getCompany().getName());
+
+		super.getResponse().setData(tuple);
+	}
 }
