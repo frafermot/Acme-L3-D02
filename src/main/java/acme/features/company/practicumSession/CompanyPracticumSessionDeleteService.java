35b7c1ab05e9c2ec0439dbdacfebeed4b3bec899
Francisco Fernández Mota <frafermot@alum.us.es>
2023-04-21 20:45:46 +0200

    delete session practicum

--- src/main/java/acme/features/company/practicumSession/CompanyPracticumSessionDeleteService.java
+++ src/main/java/acme/features/company/practicumSession/CompanyPracticumSessionDeleteService.java
@@ -4,7 +4,9 @@ package acme.features.company.practicumSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import acme.entities.practicum.Practicum;
 import acme.entities.practicumSession.PracticumSession;
+import acme.framework.components.models.Tuple;
 import acme.framework.services.AbstractService;
 import acme.roles.Company;
 
@@ -13,4 +15,72 @@ public class CompanyPracticumSessionDeleteService extends AbstractService<Compan
 
 	@Autowired
 	protected CompanyPracticumSessionRepository repository;
+
+
+	@Override
+	public void check() {
+		boolean status;
+
+		status = super.getRequest().hasData("id", int.class);
+
+		super.getResponse().setChecked(status);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status;
+		int id;
+		Practicum practicum;
+		Company company;
+		PracticumSession practicumSession;
+
+		company = this.repository.findCompanyByUserId(super.getRequest().getPrincipal().getAccountId());
+		id = super.getRequest().getData("id", int.class);
+		practicumSession = this.repository.findSessionById(id);
+		practicum = practicumSession.getPracticum();
+		status = practicum != null && practicumSession != null && super.getRequest().getPrincipal().hasRole(Company.class) && practicum.getCompany().getId() == company.getId();
+
+		super.getResponse().setAuthorised(status && !practicum.isPublished() && !practicumSession.isPublished());
+	}
+
+	@Override
+	public void load() {
+		PracticumSession object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findSessionById(id);
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void bind(final PracticumSession object) {
+		assert object != null;
+
+		super.bind(object, "title", "sessionAbstract", "periodStart", "periodEnd", "link", "published");
+	}
+
+	@Override
+	public void validate(final PracticumSession object) {
+		assert object != null;
+	}
+
+	@Override
+	public void perform(final PracticumSession object) {
+		assert object != null;
+
+		this.repository.delete(object);
+	}
+
+	@Override
+	public void unbind(final PracticumSession object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "title", "sessionAbstract", "periodStart", "periodEnd", "link", "published");
+
+		super.getResponse().setData(tuple);
+	}
 }
